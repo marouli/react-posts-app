477aea6bbf050bcdfb5a934378b54ff4ed975729
Maria Sidiropoulou <maria@stamkracht.com>
2018-06-04 20:20:30 +0200

    Refactored new MenuBoard and Menu components

--- src/components/MenuBoard/Menu/Menu.js
+++ src/components/MenuBoard/Menu/Menu.js
@@ -5,23 +5,31 @@ class Menu extends Component {
   constructor(props) {
     super(props);
     this.state = {
-    }
+      isVisible: false,
+    };
   }
 
   render() {
-    // let visibility = this.props.visibility ? "show" : "hide";
     let menus = this.props.menus;
     let childrenMenus = menus.filter(menu => menu.parentId === this.props.data.id);
-    return (
-      <ul>
-        <li><a href="#">{this.props.data.label}</a></li>
+    let submenu = "";
+    if (childrenMenus.length !== 0) {
+      submenu =
+        <ul className="c-menu-submenu--hide">
         {childrenMenus.map(menu =>
-          <Menu
-            key={menu.id}
-            data={menu}
-            menus={menus}
-          />)}
-      </ul>
+          <li>
+            <Menu
+              key={menu.id}
+              data={menu}
+              menus={menus}/>
+          </li>)}
+        </ul>
+      }
+    return (
+      <React.Fragment>
+        <a href="#">{this.props.data.label}</a>
+        {submenu}
+      </React.Fragment>
     );
   }
 }
--- src/components/MenuBoard/MenuBoard.js
+++ src/components/MenuBoard/MenuBoard.js
@@ -6,8 +6,17 @@ class MenuBoard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      menus: []
-    }
+      menus: [],
+      isVisible: false,
+    };
+
+    this.toggleVisibility = this.toggleVisibility.bind(this);
+  }
+
+  toggleVisibility() {
+    this.setState((oldState, props) => ({
+      isVisible: !oldState.isVisible
+    }));
   }
 
   componentDidMount() {
@@ -20,16 +29,30 @@ class MenuBoard extends Component {
   render() {
     let menus = this.state.menus;
     let parentMenus = menus.filter(menu => menu.parentId === null);
-    return (
-      <nav className="s-menu">
-        <img src='./img/sk.png' className="s-menu__logo" alt="logo"/>
+    let visibility = this.toggleVisibility ? "--show" : "--hide";
+    let nav =
+      <nav className="s-menu__nav" role="navigation">
+        <ul className="s-menu__ul">
         {parentMenus.map(menu =>
-          <Menu
+          <li>
+            <Menu
             key={menu.id}
             data={menu}
             menus={menus}
-          />)}
+            visibility={visibility}
+            />
+          </li>)}
+        </ul>
       </nav>
+
+    return (
+      <main className="s-menu">
+        <header>
+          <a href="#" className="s-menu__menu-icon" onClick={this.toggleVisibility}>&#9776; MENU</a>
+          <img src='./img/sk.png' className="s-menu__logo" alt="logo"/>
+        </header>
+        {nav}
+      </main>
     )
   }
 }
